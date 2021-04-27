34fd8e2ea564d9c0ad49adb2952eb46c9ac09a3d
신봉근 <bg1346@naver.com>
2021-04-28 02:23:27 +0900

    draft to make scheduler, analysis internal object structure

--- routes/new_index.js
+++ routes/new_index.js
@@ -1,4 +1,6 @@
 // routes/index.js
+const axios = require('axios');
+const cron = require('node-cron');
 const express = require('express');
 const router = express.Router();
 const libKakaoWork = require('../libs/kakaoWork');
@@ -43,6 +45,12 @@ router.get('/', async (req, res, next) => {
       })
     ),
   ]);
+
+  cron.schedule('*/4 * * * * *', () => {
+    axios.get("https://swm-chatbot-ovnwx9-6ee.run.goorm.io/test_alarm");
+  }, {
+    timezone: "Asia/Seoul"
+  }).start();
 
   // 응답값은 자유
   res.json({
@@ -149,10 +157,23 @@ router.post('/callback', async (req, res, next) => {
 router.get('/test_alarm', async (req, res, next) => {
   // 유저 목록 검색
   const users = await libKakaoWork.getUserList();
+  const date = new Date();
+  const h = date.getHours();
   // 각 유저에게 채팅방 생성
-  const conversations = await Promise.all(
-    users.map((user) => libKakaoWork.openConversations({ userId: user.id }))
-  );
+  var conversations = new Array();
+  var userkeys = Object.keys(users);
+  for (var i=0; i<userkeys.length; i++) {
+		console.log(users[userkeys[i]]);
+		var conversation = await libKakaoWork.openConversations({ userId: users[userkeys[i]].id });
+    conversations.push(conversation);
+		console.log(conversation);
+		// console.log(Object.keys(conversation));
+  }
+  // const conversations = await Promise.all(
+  //   users.map((user) => 
+  //     libKakaoWork.openConversations({ userId: user.id })
+  //   )
+  // );
   // 화면 1(메세지)
   const messages = await Promise.all([
     conversations.map((conversation) =>
