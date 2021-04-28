e55e9b7d8ff85ffff9811c97ad75401b73bc69bb
DrMaemi <leesh1510@ajou.ac.kr>
2021-04-28 22:13:47 +0900

    implements Map DS for counting the # of rejects - /libs/db/on_memory.js

--- libs/db/on_memory.js
+++ libs/db/on_memory.js
@@ -1,11 +1,26 @@
 // libs/db/on_memory.js
-exports.user_times = new Array();
-for (var i=0; i<24; i++) this.user_times.push(new Set());
+
+/* rejects: the number user clicked 'reject' on alarm */
+exports.rejects = new Map();
+
+exports.getRejects = (con_id) => {
+  return this.rejects.has(con_id)? this.rejects.get(con_id): null;
+};
+
+exports.setRejects = (con_id, r) => {
+  this.rejects.set(con_id, r);
+};
+
+
+/* cons: Conversation ids */
+exports.cons = new Array();
+// initialization
+for (var i=0; i<24; i++) this.cons.push(new Set());
 
 exports.getCons = (t) => {
-  return this.user_times[t].size? this.user_times[t]: null;
+  return this.cons[t].size? this.cons[t]: null;
 };
 
 exports.addCon = (t, con_id) => {
-  this.user_times[t].add(con_id);
+  this.cons[t].add(con_id);
 };
--- routes/new_index.js
+++ routes/new_index.js
@@ -47,7 +47,7 @@ router.get('/', async (req, res, next) => {
   ]);
 
   cron.schedule('*/4 * * * * *', () => {
-    axios.get("https://swm-chatbot-ovnwx9-6ee.run.goorm.io/test_alarm");
+    axios.get("https://swm-chatbot-ovnwx9-6ee.run.goorm.io/alarm");
   }, {
     timezone: "Asia/Seoul"
   }).start();
@@ -154,74 +154,55 @@ router.post('/callback', async (req, res, next) => {
 });
 
 // 지정된 시간 알림 예시
-router.get('/test_alarm', async (req, res, next) => {
+router.get('/alarm', async (req, res, next) => {
   // 유저 목록 검색
-  const rusers = await libKakaoWork.getUserList();
   const date = new Date();
   const h = date.getHours();
   const cons = db.getCons(h);
-  // 각 유저에게 채팅방 생성
-  // var conversations = new Array();
-  // var userkeys = Object.keys(users);
-  // for (var i=0; i<userkeys.length; i++) {
-	// 	console.log(users[userkeys[i]]);
-	// 	var conversation = await libKakaoWork.openConversations({ userId: users[userkeys[i]].id });
-  //   conversations.push(conversation);
-	// 	console.log(conversation);
-  // }
+
 	if (cons != null) {
-		// users.forEach(async (user) => {
-		// 	console.log("user:"+user);
-		// 	console.log("rusers['0'].id:"+rusers['0'].id);
-		// 	var conversation = await libKakaoWork.openConversations({ userId: user });
-		// 	conversations.push(conversation);
-		// 	console.log(conversation);
-		// });
-  
-  // 화면 1(메세지)
-  const messages = await Promise.all([
-    // cons.map((con) =>
-		cons.forEach((con_id, dump, setObject) =>
-      libKakaoWork.sendMessage({
-        conversationId: con_id,
-        text: '오늘 뭐입지?',
-        blocks: [
-          {
-            type: 'header',
-            text: '<날짜> 은 뭐입지',
-            style: 'yellow',
-          },
-		      {
-            type : "image_link",
-            url : "https://swm-chatbot-ovnwx9-6ee.run.goorm.io/resources/today_cloth1.png"
-          },
-		      {
-            type : "image_link",
-            url : "https://swm-chatbot-ovnwx9-6ee.run.goorm.io/resources/today_cloth2.png"
-          },
-          {
-            type: 'text',
-            text: '이거 입어라',
-            markdown: true,
-          },
-          {
-            type: 'button',
-            action_type: 'call_modal',
-            value: '',
-            text: '딴거 줘',
-            style: 'default',
-          },
-		      {
-            type: 'button',
-            action_type: 'call_modal',
-            value: '시간 설정하기',
-            text: '알림 시간 바꾸기',
-            style: 'default',
-          },
-        ],
-      })
-    ),
-  ]);
+    const messages = await Promise.all([
+      cons.forEach((con_id, dump, setObject) =>
+        libKakaoWork.sendMessage({
+          conversationId: con_id,
+          text: '오늘 뭐입지?',
+          blocks: [
+            {
+              type: 'header',
+              text: '<날짜> 은 뭐입지',
+              style: 'yellow',
+            },
+            {
+              type : "image_link",
+              url : "https://swm-chatbot-ovnwx9-6ee.run.goorm.io/resources/today_cloth1.png"
+            },
+            {
+              type : "image_link",
+              url : "https://swm-chatbot-ovnwx9-6ee.run.goorm.io/resources/today_cloth2.png"
+            },
+            {
+              type: 'text',
+              text: '이거 입어라',
+              markdown: true,
+            },
+            {
+              type: 'button',
+              action_type: 'call_modal',
+              value: '',
+              text: '딴거 줘',
+              style: 'default',
+            },
+            {
+              type: 'button',
+              action_type: 'call_modal',
+              value: '시간 설정하기',
+              text: '알림 시간 바꾸기',
+              style: 'default',
+            },
+          ],
+        })
+      ),
+    ]);
 	}
   // 응답값은 자유
   res.json({
